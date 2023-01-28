5d7701ae7f33418cbbb1251a1147512094db4dcd
Up-Min <korealee95@naver.com>
2023-01-28 14:46:59 +0900

    1.28

---
+++ trable/src/main/java/com/trable/TrableApplication.java
@@ -0,0 +1,13 @@
+package com.trable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrableApplication.class, args);
+	}
+
+}
---
+++ trable/src/test/java/com/trable/TrableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
