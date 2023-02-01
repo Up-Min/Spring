0fa53fa6ab03462fbdc3b5d3c9e3e281e206a3b9
Up-Min <korealee95@naver.com>
2023-02-01 18:34:15 +0900

    2.1

--- trable/src/main/java/com/trable/controller/MainController.java
+++ trable/src/main/java/com/trable/controller/MainController.java
@@ -12,26 +12,36 @@ public class MainController {
 	
 	@GetMapping(value = "/")
 	public String main() {
-		
 		return "main";
-
 	}
 	
 	@GetMapping(value = "/new")
 	public String login() {
-		
 		return "/member/loginpage";
 	}
 
 	@GetMapping(value = "/write")
 	public String write() {
-		
 		return "/user/writingpage";
 	}
 	
 	@GetMapping(value = "/view")
 	public String dtlpage() {
 		return "/travel/dtlpage";
+	}
+	
+	@GetMapping(value = "/user")
+	public String userpage() {
+		return "/user/userpage";
+	}
+	
+	@GetMapping(value = "/find")
+	public String searchpage() {
+		return "/travel/searchpage";
+	}
+	@GetMapping(value = "/like")
+	public String likepage() {
+		return "/travel/likepage";
 	}
 	
 }
